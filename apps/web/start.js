6909848bc25a8fc0432531dfc0f613defd9a471b
Sushant KC <sushantkc987@gmail.com>
2026-04-06 14:02:26 +0545

    fix: chdir to standalone dir so static files resolve correctly

--- apps/web/start.js
+++ apps/web/start.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 process.env.PORT = process.env.PORT || '3000';
 process.env.HOSTNAME = '0.0.0.0';
-require('./.next/standalone/apps/web/server.js');
+
+const path = require('path');
+// Change working directory to standalone output so static files resolve correctly
+process.chdir(path.join(__dirname, '.next', 'standalone', 'apps', 'web'));
+require('./server.js');
