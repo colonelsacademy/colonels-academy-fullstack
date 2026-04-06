90b7050c040103589202fb2ee0ba11476686a01f
Sushant KC <sushantkc987@gmail.com>
2026-04-06 13:50:27 +0545

    fix: copy static files into standalone output for CSS/JS to load

--- apps/web/start.js
+++ apps/web/start.js
@@ -1,16 +1,4 @@
 #!/usr/bin/env node
-// Railway start wrapper - ensures PORT and HOSTNAME are set for Next.js standalone
 process.env.PORT = process.env.PORT || '3000';
 process.env.HOSTNAME = '0.0.0.0';
-
-const path = require('path');
-
-// In monorepo, standalone output is at .next/standalone/apps/web/server.js
-// The process runs from /app/apps/web so we need the relative path
-const serverPath = path.join(__dirname, '.next', 'standalone', 'apps', 'web', 'server.js');
-
-console.log('Starting Next.js server from:', serverPath);
-console.log('PORT:', process.env.PORT);
-console.log('HOSTNAME:', process.env.HOSTNAME);
-
-require(serverPath);
+require('./.next/standalone/apps/web/server.js');
